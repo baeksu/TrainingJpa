b7f0079705cc6147fe4f1b6afcb4ac27f0ff9115
이경우 <kyungwoo@igyeong-uui-MacBookPro.local>
2022-05-15 12:27:06 +0900

    test2

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+    private Long memberId;
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)//순서가 바뀌거나 하면 장애가 발생할 수도 있기때문에!! String!!!
+    private OrderStatus status;
+
+}
